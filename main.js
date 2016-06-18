4901da477b3550e07f171615ddad9a6185ecd2f7
Benjamin Shaw <b.shaw@b-shaw-ltm.internal.salesforce.com>
2016-06-18 14:20:21 -0700

    changed interface to declarative classes

--- main.js
+++ main.js
@@ -34,26 +34,167 @@ my_interface = (function() {
 
     };
 
+    var mergeState = function(state1, state2) {
+        state2.lines.forEach(function(line){
+            state1.lines.push(line);
+        });
+        return state1;
+    }
+
+    var LineAnim = function(start, end, duration, remain){
+        //start and end are [x1%, y1%, x2%, y2%]
+        // duration is how long we transition from start to end
+        // remain is if we keep the line after duration is finished or delete it
+        this.start = start;
+        this.end = end;
+        this.totDuration = duration;
+        this.remain = remain;
+    };
+    LineAnim.prototype.get = function(duration) {
+        var perc = duration / this.totDuration;
+        if (perc <= 1) {
+            return {
+                lines: [[
+                this.start[0]*(1-perc) + this.end[0]*perc,
+                this.start[1]*(1-perc) + this.end[1]*perc,
+                this.start[2]*(1-perc) + this.end[2]*perc,
+                this.start[3]*(1-perc) + this.end[3]*perc,
+            ]]
+            }
+        } else {
+            if (this.remain) {
+                return {
+                    lines: [this.end]
+                }
+            } else {
+                return {lines: []}
+            }
+        }
+    }
+    LineAnim.prototype.done = function(duration) {
+        return duration > this.totDuration;
+    }
+    LineAnim.prototype.duration = function() {
+        return this.totDuration;
+    }
+
+    var SimulAnim = function(animations, duration, remain){
+        this.animations = animations;
+        this.remain = remain;
+    };
+    SimulAnim.prototype.get = function(duration){
+        var state = {lines:[]};
+        this.animations.forEach(function(anim){
+            var stateToAdd = anim.get(duration);
+            stateToAdd.lines.forEach(function(line){
+                state.lines.push(line);
+            });
+        });
+        return state;
+    }
+    SimulAnim.prototype.done = function(duration){
+        this.animations.forEach(function(anim){
+            if (!anim.done(duration)) {
+                return false;
+            }
+        })
+        return true;
+    }
+    SimulAnim.prototype.duration = function() {
+        var d = 0;
+        this.animations.forEach(function(anim){
+            d = Math.max(d, anim.duration());
+        });
+        return d;
+    }
+
+    var ConsecAnim = function(animations){
+        this.animations = animations;
+        this.lastStart = 0;
+    };
+    ConsecAnim.prototype.get = function(duration){
+        var currState = {lines:[]}
+        for (var i=0; i<this.animations.length; i++){
+            var d = this.animations[i].duration();
+            if (duration > d){
+                if (this.animations[i].remain) {
+                    var addState = this.animations[i].get(duration);
+                    currState = mergeState(currState, addState);
+                }
+                duration -= d;
+            } else {
+                var addState = this.animations[i].get(duration);
+                currState = mergeState(currState, addState);
+                break;
+            }
+        }
+        return currState;
+    }
+    var WaitAnim = function(duration){
+
+    };
+
+    var centExpandAnim = new SimulAnim(
+        [new LineAnim(
+            [0.5, 0.4, 0.5, 0.6],
+            [0.5-0.15, 0.4-0.15, 0.5-0.15, 0.6+0.15],
+            5000,
+            true
+        ),
+        new LineAnim(
+            [0.5, 0.4, 0.5, 0.6],
+            [0.5+0.15, 0.4-0.15, 0.5+0.15, 0.6+0.15],
+            5000,
+            true
+        )],
+        null,
+        true
+    )
+    var covExpandAnim = new SimulAnim(
+        [new LineAnim(
+            [0.35, 0.2, 0.35, 0.2],
+            [0.35-0.05, 0.2, 0.35+0.05, 0.2],
+            1000,
+            true
+        ),
+        new LineAnim(
+            [0.65, 0.2, 0.65, 0.2],
+            [0.65-0.05, 0.2, 0.65+0.05, 0.2],
+            1000,
+            true
+        ),
+        new LineAnim(
+            [0.35, 0.8, 0.35, 0.8],
+            [0.35-0.05, 0.8, 0.35+0.05, 0.8],
+            1000,
+            true
+        ),
+        new LineAnim(
+            [0.65, 0.8, 0.65, 0.8],
+            [0.65-0.05, 0.8, 0.65+0.05, 0.8],
+            1000,
+            true
+        ),
+        ],
+        null,
+        true
+    )
+    var root = new ConsecAnim([centExpandAnim,covExpandAnim])
+
     var main = function() {
         console.log("running main"); //skrifar i console
         var canvas = document.getElementById('myCanvas');
         var context = canvas.getContext('2d');
 
         var state = {
-            lines: [
-                [0.5, 0.4, 0.5, 0.6], ///essi lina a ad hverfa
-                [0.5, 0.4, 0.5, 0.6],
-                [0.5, 0.4, 0.5, 0.6],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-            ]
+            lines: []
+        }
+        var lastState = {
+            lines: []
         }
         var needsRedraw = true;
 
         var startTime = new Date();
-        var lastTime = new Date();
 
         function initialize() {
             window.addEventListener('resize', resizeCanvas, false);
@@ -66,50 +207,25 @@ my_interface = (function() {
             var currentTime = new Date();
             var sinceStart = currentTime - startTime;
 
-            var evenSecond = sinceStart % 2000 < 1000;
-            needsRedraw = false;
-            var waitTime = 50;
-            if (sinceStart < 5000 + waitTime && sinceStart > waitTime) {
-                var prop = ((sinceStart - waitTime) / 5000)
-                    //change x
-                state.lines[1][0] = 0.5 - (0.15 * prop)
-                state.lines[1][2] = 0.5 - (0.15 * prop)
-                state.lines[2][0] = 0.5 + (0.15 * prop)
-                state.lines[2][2] = 0.5 + (0.15 * prop)
-
-                //change y
-                state.lines[1][1] = 0.4 - (0.15 * prop)
-                state.lines[1][3] = 0.6 + (0.15 * prop)
-                state.lines[2][1] = 0.4 - (0.15 * prop)
-                state.lines[2][3] = 0.6 + (0.15 * prop)
-                needsRedraw = true;
-            }
-            if (sinceStart > 5000 + waitTime && state.lines[3][0] == 0) {
-                state.lines[3] = [0.3, 0.2, 0.4, 0.2]
-                state.lines[4] = [0.6, 0.2, 0.7, 0.2]
-                state.lines[5] = [0.3, 0.8, 0.4, 0.8]
-                state.lines[6] = [0.6, 0.8, 0.7, 0.8]
-                needsRedraw = true;
-            }
-            if (sinceStart > 5000 + waitTime && state.lines[3][0] != 0 && sinceStart < 6000 + waitTime) {
-                var prop = ((sinceStart - waitTime - 5000) / 1000)
-                state.lines[3][0] = 0.35 - (0.05 * prop)
-                state.lines[3][2] = 0.35 + (0.05 * prop)
-                state.lines[4][0] = 0.65 - (0.05 * prop)
-                state.lines[4][2] = 0.65 + (0.05 * prop)
-                state.lines[5][0] = 0.35 - (0.05 * prop)
-                state.lines[5][2] = 0.35 + (0.05 * prop)
-                state.lines[6][0] = 0.65 - (0.05 * prop)
-                state.lines[6][2] = 0.65 + (0.05 * prop)
-
-                needsRedraw = true;
-            }
-
-            lastTime = currentTime
+            state = root.get(sinceStart);
 
         }
 
         function redraw() {
+
+            var needsRedraw = false
+                if (state.lines.length !== lastState.lines.length) {
+                    needsRedraw = true;
+                } else {
+                    for (var i = 0; i < state.lines.length; i++) {
+                        for (var j = 0; j < 4; j++){
+                            if (state.lines[i][j] !== lastState.lines[i][j]) {
+                                needsRedraw = true;
+                            }
+                        }
+                    }
+                }
+
             if (!needsRedraw) {
                 return
             }
@@ -127,7 +243,7 @@ my_interface = (function() {
                 context.stroke();
 
             });
-            needsRedraw = false;
+            lastState = state;
 
         }
 
